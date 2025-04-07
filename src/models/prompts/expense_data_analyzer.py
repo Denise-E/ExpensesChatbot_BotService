17d52299c9a7784642b446e8edfb0aa7ee21846e
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2025-04-07 13:25:45 -0300

    expense_analyzer promp and data updated

--- src/models/prompts/expense_data_analyzer.py
+++ src/models/prompts/expense_data_analyzer.py
@@ -5,12 +5,28 @@ data_analyzer_prompt = PromptTemplate(
     template="""
 You are an assistant that extracts expense details from user messages.
 
-Analyze the message and extract:
-- amount: the total numeric amount spent (e.g., 1500). If no amount is found, return null.
-- description: a short description of the item, service, or reason for the expense (e.g., "a new phone", "lunch", "Netflix subscription").
+Return a JSON object with: - "amount": the total amount spent, written as a **string** with decimal point (e.g., 
+"8.0", "0.05", "3.75", "1.50", "30.50"). Use as many decimal digits as necessary (e.g., "0.05" for 5 cents). If the 
+amount is in cents, convert to dollars. Multiply only if the total is not already mentioned. - "description": a 
+concise summary of the expense (e.g., "Netflix subscription", "Books", "Lunch"). Only the first letter in capital and 
+without declarations between parenthesis.
 
-Respond only with a JSON object.
+Format:
+{{
+  "amount": "string or null",
+  "description": "string"
+}}
+
+Examples:
+"5 cents, to enter the museum" → amount: "0.05", description: "Museum"
+"Went bowling and paid for two rounds 10 bucks" → amount: "10.0", description: "Bowling"
+"I bought 2 coffees at 3.75 bucks each" → amount: "7.5", description: "Coffees"
+"Grabbed 10 stamps, 0.6 each" → amount: "6.0", description: "Stamps"
+"I got 4 school textbooks for 15 bucks each" -> amount: "60.0", description: "School textbooks"
+"Sushi delivered for dinner, 80 " → amount: "80.0", description: "Sushi"
+"Moved 1500 bucks to a high-yield savings account" → amount: "1500.0", description: "Savings account"
 
 Message: "{message}"
 """
 )
+
--- tests/models_tests/expense_analyzer_test.py
+++ tests/models_tests/expense_analyzer_test.py
@@ -13,27 +13,29 @@ async def main():
     passed = 0
     for i, example in enumerate(expense_analyzer_examples, start=1):
         try:
-            # TODO test description text
             result = await expense_extraction_pipeline.ainvoke({"message": example["input"]})
             expected = example["expected"]["amount"]
             received = result.amount
 
-            if received == expected:
+            if received == str(expected):  # Due to changes on amount type for SQLAlchemy managment
                 logger.info(f"[{i}] ✅ OK | Input: {example['input']} | Result {received}")
-                logger.info(f"Description expected -> {example["expected"]["description"]} - Description recived -> {result.description}")
+                logger.info(
+                    f"Description expected -> {example["expected"]["description"]} - Description recived -> {result.description}")
                 passed += 1
             else:
                 logger.warning(f"[{i}] ❌ FAIL | Input: {example['input']} | Result {received}")
                 logger.info(f"    ➤ Esperado: {expected}, Recibido: {received}")
-                logger.info(f"Description expected -> {example["expected"]["description"]} - Description recived -> {result.description}")
+                logger.info(
+                    f"Description expected -> {example["expected"]["description"]} - Description recived -> {result.description}")
 
         except Exception as e:
             logger.warning(f"[{i}] ❌ ERROR | Input: {example['input']} - {result.description}")
             logger.warning(f"    ➤ Excepción: {e}")
 
     examples_quantity = len(expense_analyzer_examples)
-    percentage = passed*100/examples_quantity
+    percentage = passed * 100 / examples_quantity
     logger.info(f"Successfull tests: {passed} out of {examples_quantity} - Equals a {round(percentage, 2)}% "
                 f"assertion")
+
 
 asyncio.run(main())
